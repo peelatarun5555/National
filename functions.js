bcaeddfe708f9d8a8f8aafa5063868314b41112c
peelatarun5555 <43408186+peelatarun5555@users.noreply.github.com>
2024-08-14 21:57:16 +0530

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,6 +1,6 @@
  
 
-var $window = $(window), gardenCtx, gardenCanvas, $garden, garden;
+var $window = $(window), gardenCtx, gardenCanvas, $garden, $garden;
 var clientWidth = $(window).width();
 var clientHeight = $(window).height();
 $(function () {  
